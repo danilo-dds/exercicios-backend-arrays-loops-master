635038412b3d6e089ef8f416ec21f475445d003d
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-04 22:26:39 -0300

    exercicio 05 feito

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,8 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 4, 12, 21, 53, 88, 112, 10, 16];
+const numerosPares = []
+for (let numero of original) {
+    if (numero % 2 == 0) {
+        numerosPares.push(numero)
+    }
+}
+console.log(numerosPares)
