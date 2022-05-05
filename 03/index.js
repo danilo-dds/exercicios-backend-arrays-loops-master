735ec8e3a97d2f937f95df2a8fb1105538e6e717
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-04 22:01:33 -0300

    exercicio 03 feito

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,10 @@
-const numeros = [54, 22, 14, 87, 10, 284];
-
+const numeros = [78, 19, 14, 87, 11, 284];
+let resporta = -1;
+for (let i = 0; i < numeros.length; i++) {
+    let numero = numeros[i];
+    if (numero === 10) {
+        resporta = i
+        break
+    }
+}
+console.log(resporta)
