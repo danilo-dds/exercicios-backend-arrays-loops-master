edf08113ddd1692370b9f84633e76517d94c65c2
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-05 06:16:22 -0300

    exercicio 07 feito

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,7 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ["Ana", "Joana", "Carlos", "amanda", "Antonio"];
+const nomesIniciadosComAa = []
+for (let i of nomes) {
+    if (i[0] === "a" || i[0] === "A")
+        nomesIniciadosComAa.push(i)
+}
+console.log(nomesIniciadosComAa)
