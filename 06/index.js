47ef16ddf73763b64a3a33c836558a02dfe20103
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-04 22:31:28 -0300

    exercicio 06 feito

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,8 @@
-const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
-
+const numeros = [3, 4, 7, 8, 1, 6, 5, 10, 2];
+let soma = 0;
+for (let par of numeros) {
+    if (par % 2 == 0) {
+        soma += par
+    }
+}
+console.log(soma)
