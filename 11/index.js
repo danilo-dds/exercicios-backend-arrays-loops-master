fde2bb9df7420b4f60b30eca9f7e74c293fdc840
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-06 06:18:28 -0300

    exercicio 11 feito

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,8 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
-
+const original = [5, 7, 13, 17, 26, 34, 118, 245, 19, 101];
+const numeros = [];
+for (let numero of original) {
+    if (numero >= 10 && numero <= 20 || numero >= 100) {
+        numeros.push(numero);
+    }
+}
+console.log(numeros);
