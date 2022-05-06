79d32f414986b6f2f92e9ddc38181225955c6baf
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-06 06:46:20 -0300

    exercicio 13 feito

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,7 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+for (let nunDisjuntores = 0; nunDisjuntores < disjuntores.length; nunDisjuntores++) {
+    if (disjuntores[nunDisjuntores]) {
+        console.log(nunDisjuntores);
+    }
+}
